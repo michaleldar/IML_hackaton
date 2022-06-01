a47fd9340c4e7beb8ca37d48afe6f14f106738c0
Noam Blum <noamblum756@gmail.com>
2022-06-01 21:44:33 +0300

    Added handling of Ivi_Lymphovascular_invasion

--- preprcessing.py
+++ preprcessing.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
+def string2boolean(string):
+    POSITIVE_STRINGS = {"pos", "+", "extensive", "micropapillary variant", "yes", "(+)"}
+    NEGATIVE_STRINGS = {"none", "-", "no", "(-)", "neg", "not"}
+    if pd.isna(string) or string in NEGATIVE_STRINGS:
+        return 0
+    elif string in POSITIVE_STRINGS:
+        return 1
+    return string 
+
+
 def preprocessing(df: pd.DataFrame):
-    pass
+    # Convert Ivi_Lymphovascular_invasion to boolean
+    df["Ivi_Lymphovascular_invasion"] = df["Ivi_Lymphovascular_invasion"].apply(string2boolean)
 
 if __name__ == "__main__":
     preprocessing(pd.read_csv("data/train.feats.csv"))
