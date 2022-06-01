142433b8ab98492d5cdc95a4871bb633629cd75d
michalel <michal.eldar1@mail.huji.ac.il>
2022-06-01 22:28:42 +0300

    preprocessing histopatological_degree

--- preprcessing.py
+++ preprcessing.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
+
+def histopatological_degree_to_int(degree: str):
+    values = ["null", "gx", "g1", "g2", "g3", "g4"]
+    for idx, val in enumerate(values):
+        if val in degree.lower():
+            return idx
+    return 0
+
+
 def preprocessing(df: pd.DataFrame):
     pass
+
 
 if __name__ == "__main__":
     preprocessing(pd.read_csv("data/train.feats.csv"))
