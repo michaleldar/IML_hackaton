7a8be9c3074d8c0497c83288cbbd736b1e5a081b
Noam Blum <noamblum756@gmail.com>
2022-06-01 23:30:51 +0300

    Finished Ki67 processing

--- preprcessing.py
+++ preprcessing.py
@@ -10,6 +10,32 @@ def string2boolean(string):
         return 1
     return string 
 
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    from dateutil.parser import parse
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+
+
+def handle_ki67(string):
+    if type(string) != str: return string
+    if is_date(string): return pd.NA
+
+    # Find largest number in string
+    nums = [int(x) for x in re.findall(r'\d+', string)]
+    if len(nums) == 0: return pd.NA
+    return max(nums)
+
+
 
 def histopatological_degree_to_int(degree: str):
     values = ["null", "gx", "g1", "g2", "g3", "g4"]
@@ -24,10 +50,14 @@ def preprocessing(df: pd.DataFrame):
     # Convert Ivi_Lymphovascular_invasion to boolean
     df["Ivi_Lymphovascular_invasion"] = df["Ivi_Lymphovascular_invasion"].apply(string2boolean)
     # Convert Histopatological degree to int (greater should be more correlated to cancer)
-    df["Histopatological degree"] = df["Histopatological degree"].apply(histopatological_degree_to_int)
+    df["Histopatological_degree"] = df["Histopatological_degree"].apply(histopatological_degree_to_int)
     # Num of surgeries in int (Na = 0)
-    df['Surgery sum'] = df['Surgery sum'].fillna(0)
+    df['Surgery_sum'] = df['Surgery_sum'].fillna(0)
 
+
+    df["KI67_protein"] = df["KI67_protein"].apply(handle_ki67)
+    df['KI67_protein'].fillna((df['KI67_protein'].mean()), inplace=True)
+    print(df['KI67_protein'].value_counts())
 
 if __name__ == "__main__":
     preprocessing(pd.read_csv("data/train.feats.csv"))
