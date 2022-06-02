6022bd69f8b6c850561a0d648c3f109cd93ed13a
Noam Blum <noamblum756@gmail.com>
2022-06-02 21:47:33 +0300

    Use RakelD for multilabel classification

--- task2/classifier.py
+++ task2/classifier.py
@@ -2,6 +2,7 @@ import sklearn.linear_model
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
+from skmultilearn.ensemble import RakelD
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -53,11 +54,11 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = "."):
 
 if __name__ == '__main__':
 
-    X = pd.read_csv("../data/train.feats.csv")
-    y = pd.read_csv("../data/train.labels.0.csv")
+    X = pd.read_csv("data/train.feats.csv")
+    y = pd.read_csv("data/train.labels.0.csv")
 
-    train_X = X.sample(frac=0.5, random_state=42)
-    train_y = y.sample(frac=0.5, random_state=42)
+    train_X = X.sample(frac=0.5, random_state=12344)
+    train_y = y.sample(frac=0.5, random_state=12344)
     test_X = X.drop(train_X.index)
     test_y = y.drop(train_y.index)
 
@@ -68,27 +69,36 @@ if __name__ == '__main__':
 
     # vizualization_for_features(train_X)
     # feature_evaluation(train_X, train_y)
-    lr = RandomForestClassifier()
-    y_pred = pd.DataFrame()
+    rk = RakelD(
+        base_classifier = RandomForestClassifier(),
+        base_classifier_require_dense=[True, True]
+    )
+    rk.fit(train_X, train_y)
+    pred = rk.predict(test_X)
     y_gold = pd.DataFrame()
+    y_pred = pd.DataFrame.sparse.from_spmatrix(pred, columns=train_y.columns).sparse.to_dense()
 
-    for column in train_y.columns:
-        lr.fit(train_X, train_y[column])
-        y_pred[column] = lr.predict(test_X)
-        # pd.DataFrame(y_pred).to_csv("./y_pred.csv", index=False, header=False)
-        # pd.DataFrame(test_y["LYM - Lymph nodes"]).to_csv("y_gold.csv", index=False, header=False)
-        # print(lr.score(test_X, test_y["LYM - Lymph nodes"]))
     y_pred["prediction"] = y_pred.apply(lambda x: str([c for c in x.index if x[c] > 0]), axis=1)
-    # y_pred = pd.merge(test_y, y_pred, how="left", on="id")
-    y_gold["prediction"] = test_y[test_y.columns[0]] #test_y.apply(lambda x: str([c for c in x.index if x[c] > 0]), axis=1)
+    y_gold["prediction"] = test_y[test_y.columns[0]]
     y_pred["prediction"].to_csv("./y_pred.csv", header=False, index=False)
     y_gold["prediction"].to_csv("./y_gold.csv", header=False, index=False)
-    empty = ['[]'] * y_gold.shape[0]
-    pd.DataFrame(empty).to_csv('./y_empty.csv', header=False, index=False)
 
-    X, y = preprocessing_train(pd.read_csv("../data/train.feats.csv"), pd.read_csv("../data/train.labels.1.csv"), multi_label=False)
-    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.5, random_state=42)
+
+    y = pd.read_csv("data/train.labels.1.csv")
+    train_X = X.sample(frac=0.5, random_state=42)
+    train_y = y.sample(frac=0.5, random_state=42)
+    test_X = X.drop(train_X.index)
+    test_y = y.drop(train_y.index)
+
+    # X, y = preprocessing_train(pd.read_csv("data/train.feats.csv"), pd.read_csv("data/train.labels.0.csv"))
+    # train_X, test_X, train_y, test_y = train_test_split(X,y,test_size=0.5, random_state=42)
+    train_X, train_y = preprocessing_train(train_X, train_y, False)
+    test_X = preprocessing_test(test_X)
+
+    # X, y = preprocessing_train(pd.read_csv("../data/train.feats.csv"), pd.read_csv("../data/train.labels.1.csv"), multi_label=False)
+    # train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.5, random_state=42)
     lr = LinearRegression()
     lr.fit(train_X, train_y)
     pred = lr.predict(test_X)
-    print(sklearn.metrics.mean_squared_error(pred, test_y))
+    print("Std:", test_y["אבחנה-Tumor size"].std())
+    print("RMSE:", np.sqrt(sklearn.metrics.mean_squared_error(pred, test_y)))
